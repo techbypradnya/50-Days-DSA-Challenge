bac6d4889aaf09a64e3d51d929d6559dc6eb7e03
techbypradnya <pradnyapangavhane23@gmail.com>
2026-06-10 16:45:57 +0530

    Duplicate Search Code Completed

---
+++ Day-01/Duplicate.java
@@ -0,0 +1,17 @@
+public class Duplicate {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 20, 40, 10, 50};
+
+        System.out.println("Duplicate elements are:");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+    }
+}
