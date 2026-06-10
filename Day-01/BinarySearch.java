cb65d4e3873cdea002feeb85272925fa0f1fce7d
techbypradnya <pradnyapangavhane23@gmail.com>
2026-06-10 16:49:38 +0530

    code updated

--- Day-01/BinarySearch.java
+++ Day-01/BinarySearch.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 
 public class BinarySearch {
     public static void main(String[] args) {
+
         int[] arr = {10, 20, 30, 40, 50, 60, 70};
 
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter element to search: ");
         int key = sc.nextInt();
 
         int low = 0;
         int high = arr.length - 1;
-        boolean found = false;
+        int position = -1;
 
         while (low <= high) {
             int mid = (low + high) / 2;
 
             if (arr[mid] == key) {
-                System.out.println("Element found at index " + mid);
-                found = true;
+                position = mid;
                 break;
             } else if (key < arr[mid]) {
                 high = mid - 1;
@@ -26,8 +27,13 @@ public class BinarySearch {
             }
         }
 
-        if (!found) {
+        if (position != -1) {
+            System.out.println("Element found at index " + position);
+            System.out.println("Position = " + (position + 1));
+        } else {
             System.out.println("Element not found");
         }
+
+        sc.close();
     }
 }
