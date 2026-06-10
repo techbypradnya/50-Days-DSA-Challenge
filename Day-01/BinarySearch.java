4d3d40958cc1d123bc6becd0efeae597f24eb351
techbypradnya <pradnyapangavhane23@gmail.com>
2026-06-10 16:44:52 +0530

    Binary Search Code Completed

--- Day-01/BinarySearch.java
+++ Day-01/BinarySearch.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+
 public class BinarySearch {
-    
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter element to search: ");
+        int key = sc.nextInt();
+
+        int low = 0;
+        int high = arr.length - 1;
+        boolean found = false;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                System.out.println("Element found at index " + mid);
+                found = true;
+                break;
+            } else if (key < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+    }
 }
