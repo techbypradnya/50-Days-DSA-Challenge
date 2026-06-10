764bf029f7c1687e0018f13f192533b6d081998f
techbypradnya <pradnyapangavhane23@gmail.com>
2026-06-10 16:49:08 +0530

    code updated

--- Day-01/linearSearch.java
+++ Day-01/linearSearch.java
@@ -2,24 +2,30 @@ import java.util.Scanner;
 
 public class linearSearch {
     public static void main(String[] args) {
+
         int[] arr = {10, 20, 30, 40, 50};
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter element to search: ");
         int key = sc.nextInt();
 
-        boolean found = false;
+        int position = -1;
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key) {
-                System.out.println("Element found at index " + i);
-                found = true;
+                position = i;
                 break;
             }
         }
 
-        if (!found) {
+        if (position != -1) {
+            System.out.println("Element found at index " + position);
+            System.out.println("Position = " + (position + 1));
+        } else {
             System.out.println("Element not found");
         }
+
+        sc.close();
     }
 }
